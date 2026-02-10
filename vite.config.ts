d47bfc8dec7f8f0cefc7bd94b068afc2bcf565b2
vaghelameet07 <vaghmeet9@gmail.com>
2026-02-10 10:39:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
+  css: {
+    postcss: './postcss.config.js',
   },
 });
